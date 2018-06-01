47d33d7e264e93d66432f3ebb5f1a48fac738d84
bqlab <beaqon@naver.com>
2018-06-02 00:23:06 +0900

    Initial commit

---
+++ build/generated/source/buildConfig/debug/com/kakao/talk/theme/ryan/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kakao.talk.theme.ryan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bqlab.themelab.sample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "1.0.0";
+}
---
+++ src/main/java/com/kakao/talk/theme/ryan/MainActivity.java
@@ -0,0 +1,50 @@
+package com.kakao.talk.theme.ryan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private static final String KAKAOTALK_SETTINGS_THEME_URI = "kakaotalk://settings/theme/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                Window window = this.getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.setStatusBarColor(getResources().getColor(R.color.statusBarColor));
+            } catch (Throwable ignored) {
+            }
+        }
+
+        if (!hasKakaoTalk()) {
+            Toast.makeText(this, "카카오톡이 설치되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+            finish();
+        } else {
+            final Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(KAKAOTALK_SETTINGS_THEME_URI + getPackageName()));
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public boolean hasKakaoTalk() {
+        try {
+            this.getPackageManager().getApplicationInfo("com.kakao.talk", 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
