a377c0f7663990c68b53c68b4fe6167870803761
bqlab <beaqon@naver.com>
2018-06-02 13:24:10 +0900

    patch-180602-2
     - Color의 StatusBarColor 제거
     - 카카오에서 만든 설치 화면으로 추정되는 기능 제거

--- src/main/java/com/kakao/talk/theme/ryan/MainActivity.java
+++ src/main/java/com/kakao/talk/theme/ryan/MainActivity.java
@@ -4,10 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -17,17 +14,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            try {
-                Window window = this.getWindow();
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                window.setStatusBarColor(getResources().getColor(R.color.statusBarColor));
-            } catch (Throwable ignored) {
-            }
-        }
-
         if (!hasKakaoTalk()) {
             Toast.makeText(this, "카카오톡이 설치되어 있지 않습니다.", Toast.LENGTH_LONG).show();
             finish();
