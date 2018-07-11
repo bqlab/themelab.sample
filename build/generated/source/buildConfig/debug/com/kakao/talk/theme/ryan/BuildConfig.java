228c33719f2cdd6265e48d310aaca6b16dbc1b31
Choi Sinho <beaqon@naver.com>
2018-07-11 22:50:52 +0900

    상타매시지

--- build/generated/source/buildConfig/debug/com/kakao/talk/theme/ryan/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/kakao/talk/theme/ryan/BuildConfig.java
@@ -5,7 +5,7 @@ package com.kakao.talk.theme.ryan;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.bqlab.themelab.sample";
+  public static final String APPLICATION_ID = "com.bqlab.themelab.c0006";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 100;
